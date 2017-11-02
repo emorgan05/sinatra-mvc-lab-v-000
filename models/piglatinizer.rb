fe7f13797f861bdd98fde0dde5a88c8264a88697
Emily Morgan <emorgan05@gmail.com>
2017-11-02 15:04:19 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,26 @@
 class PigLatinizer
+  attr_reader :user_phrase
 
+  def initialize(user_phrase)
+    @user_phrase = user_phrase.downcase
+  end
+
+  def piglatinize
+    text_array = user_phrase.split(" ")
+    piglatin = []
+
+    text_array.each do |word|
+      if word[0].scan(/[aeiou]/)
+        word += "way"
+        piglatin << word
+      else
+        index = word.index(/[aeiou]/)
+        word_core = word.slice(index..word.size)
+        word_start = word.slice(0..index)
+        pig_word = word_core + word_start + "ay"
+        piglatin << pig_word
+      end
+    end
+    piglatin.join(" ")
+  end
 end
