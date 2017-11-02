4acb2a08c6055b14e0402df226aaa8b1656032f3
Emily Morgan <emorgan05@gmail.com>
2017-11-02 15:18:36 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,7 +17,7 @@ class PigLatinizer
       else
         index = word.index(/[aeiou]/)
         word_core = word.slice(index..word.size)
-        word_start = word.slice(0..index)
+        word_start = word.slice(0..index - 1)
         pig_word = word_core + word_start + "ay"
         piglatin << pig_word
       end
