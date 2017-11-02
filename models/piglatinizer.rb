3b213f606a992ae3a9eac71e7f9ba6e1df8591c4
Emily Morgan <emorgan05@gmail.com>
2017-11-02 15:29:50 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,14 +6,13 @@ class PigLatinizer
   end
 
   def piglatinize(word)
-      if word[0].scan(/[aeiouAEIOU]/) != []
-        word += "way"
-      else
-        index = word.index(/[aeiou]/)
-        word_core = word.slice(index..word.size)
-        word_start = word.slice(0..index - 1)
-        pig_word = word_core + word_start + "ay"
-      end
+    if word[0].scan(/[aeiouAEIOU]/) != []
+      word += "way"
+    else
+      index = word.index(/[aeiou]/)
+      word_core = word.slice(index..word.size)
+      word_start = word.slice(0..index - 1)
+      pig_word = word_core + word_start + "ay"
     end
   end
 
